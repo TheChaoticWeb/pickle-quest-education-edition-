fa3ae1d40bccd573e8155a95ffe54e6b650ae189
DamienB325 <49534369+DamienB325@users.noreply.github.com>
2022-09-13 14:52:32 -0700

    Add base grass img code.

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
 const canv = document.querySelector("canvas");
 const ctx = canv.getContext("2d");
+var grass = new Image;
+grass.src = "";
 function setup() {
     requestAnimationFrame(loop);
 }
@@ -7,6 +9,7 @@ function loop() {
     requestAnimationFrame(loop);
     canv.width = innerWidth;
     canv.height = innerHeight;
+    ctx.drawImage();
 }
 
 window.onload = function() {
