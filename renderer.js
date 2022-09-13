1d298758d38ef7e2afa34bab1533c378f5fbaba0
DamienB325 <49534369+DamienB325@users.noreply.github.com>
2022-09-13 09:55:23 -0700

    Add ads.

--- renderer.js
+++ renderer.js
@@ -8,3 +8,25 @@ function loop() {
     canv.width = innerWidth;
     canv.height = innerHeight;
 }
+var Game = Object.create(null);
+Game.WaitUntil = function(apiKey, promise) {
+	promise.then(function(value) {
+		Game[apiKey] = value;
+	});
+	return promise;
+}
+Game.adManager = Game.WaitUntil("adManager", AdManager.fetchData("ads.json"));
+onkeydown = function(event) {
+	event.preventDefault();
+	if(!$("AdManager").hidden) {
+		return false;
+	}
+	if(Math.random() <= Game.adManager.data.chance) {
+		Game.adManager.loadRandomAd();
+		return false;
+	}
+	if(Game.checkInputLocked(event)) {
+		return false;
+	}
+	return false;
+}
