deb53936e344f835c764ca71ec24a731c38f4501
DamienB325 <49534369+DamienB325@users.noreply.github.com>
2022-09-15 12:45:33 -0700

    Continue moving to pixi.js.

--- game.js
+++ game.js
@@ -1,7 +1,16 @@
 const app = new Application();
 
-var grass = new Image;
-grass.src = "grass.png";
+document.body.appendChild(app.view);
+
+const grassTexture = await Assets.load('bunny.png');
+
+const grass = new Sprite(grassTexture);
+
+grass.x = app.renderer.width / 2;
+grass.y = app.renderer.height / 2;
+
+app.stage.addChild(grass);
+
 /*
 function setup() {
     requestAnimationFrame(loop);
