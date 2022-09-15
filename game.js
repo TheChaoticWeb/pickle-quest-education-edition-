1228ab0493d173bda05824092aaca6ec5afc3672
DamienB325 <49534369+DamienB325@users.noreply.github.com>
2022-09-15 12:43:26 -0700

    Move to pixi.js for renderer.

---
+++ game.js
@@ -0,0 +1,17 @@
+const app = new Application();
+
+var grass = new Image;
+grass.src = "grass.png";
+/*
+function setup() {
+    requestAnimationFrame(loop);
+}
+function loop() {
+    requestAnimationFrame(loop);
+    
+}
+
+window.onload = function() {
+    setup();
+}
+*/
